7460b5fb510385d647500f41b2d3c3b9ecc8516a
jacovb <vbiljon@gmail.com>
2022-08-09 06:29:42 +0100

    starting this thing

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero: React.FC = () => {
+  return (
+    <div>
+      <p>Welcome to the Payment Calculator</p>
+      <button>Fixed Rate Loan</button>
+      <button>Variable Rate Loan</button>
+    </div>
+  )
+}
+
+export default Hero
